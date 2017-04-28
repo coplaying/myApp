891507ad2e5b4ae79e0e212030a4ef6191bed2ad
hgyang <hyungang@gmail.com>
2017-04-28 19:50:06 +0900

    ejs set-up

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/',function (req,res) {
-  res.send('Hello World!');
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', function (req,res) {
+  res.render('hello');
+  console.log('get request');
 });
 
 app.listen(3000, function(){
