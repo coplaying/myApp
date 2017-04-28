78b2af2db34034af1cd1632ee545f00ba01d217f
hyungang <hyungang@gmail.com>
2017-04-28 22:54:16 +0900

    install mongoose

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function (req,res) {
+  res.render('main');
+});
+
 app.get('/hello', function (req,res) {
   res.render('hello');
   console.log('get request');
