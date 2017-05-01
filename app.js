57c17a3ce85aff76ab7615376cc397573e2f392c
hyungang <hyungang@gmail.com>
2017-05-01 23:53:43 +0900

    add body parser and backend coding

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //set up db connection
 mongoose.connect(process.env.MONGO_DB);
@@ -13,24 +14,54 @@ db.on("error",function (err) {
   console.log("DB ERROR :", err);
 });
 
-//create table if not exist
-var dataSchema = mongoose.Schema({
-  name:String,
-  count:Number
+//model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
 });
-var Data = mongoose.model('data',dataSchema);
-Data.findOne({name:"myData"}, function (err,data) {
-  if(err) return console.log("Data ERROR:",err);
-  if(!data){
-    Data.create({name:"myData",count:0},function (err,data) {
-      if(err) return console.log("Data ERROR:",err);
-      console.log("Counter initialized :",data);
-    });
-  }
-});
+var Post = mongoose.model('post',postSchema);
 
+//view setting
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+
+//set middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+//set routes
+app.get('/posts', function (req,res) {
+    Post.find({}, function (err,posts) {
+      if(err) return res.json({success:false, message:err});
+      res.json({success:true, data:posts});
+    });
+}); //index
+app.post('/posts', function (req,res) {
+  Post.create(req.body.post, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, data:post});
+  });
+}); //create
+app.get('/posts/:id', function (req,res) {
+  Post.findById(req.params.id, function (err,post) {
+    if(err) return res.json({success:false, messsage:err});
+    res.json({success:true, data:post});
+  });
+}); //show
+app.put('/posts/:id', function (req,res) {
+  req.body.post.updatedAt-Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post) {
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" updated"});
+  });
+}); //update
+app.delete('/posts/:id', function (req,res) {
+  Post.findByIdAndRemove(req.params.id, function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.json({success:true, message:post._id+" deleted"});
+  });
+}); //destroy
 
 app.get('/', function (req,res) {
   Data.findOne({name:"myData"},function (err,data) {
@@ -48,6 +79,7 @@ app.get('/hello', function (req,res) {
   console.log('get request');
 });
 
+//start server
 app.listen(3000, function () {
   console.log('Server on!');
 });
