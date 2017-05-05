501edb644033db854dc2bcf31d31210f248825fd
hgyang <hyungang@gmail.com>
2017-05-05 17:01:14 +0900

    passport middleware set-up

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var async = require('async');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
@@ -24,6 +28,14 @@ var postSchema = mongoose.Schema({
 });
 var Post = mongoose.model('post',postSchema);
 
+var userSchema = mongoose.Schema({
+  email: {type:String, required:true, unique:true},
+  nickname: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now}
+});
+var User = mongoose.model('user',userSchema);
+
 //view setting
 app.set("view engine", "ejs");
 
@@ -32,6 +44,45 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.use(flash());
+
+app.use(session({secret:'MySecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+var LocalStrategy = require('passport-local').Strategy;
+passport.use('local-login',
+  new LocalStrategy({
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true
+  },
+    function(req, email, password, done) {
+      User.findOne({ 'email' : email }, function(err, user) {
+        if (err) return done(err);
+
+        if (!user) {
+          req.flash("email", req.body.email);
+          return done(null, false, req.flash('loginError', 'No user found.'));
+        }
+        if (user.password != password) {
+          req.flash("email", req.body.email);
+          return done(null, false, req.flash('loginError', 'Password does not Match.'));
+        }
+        return done(null, user);
+      });
+    }
+  )
+);
 
 //set routes
 app.get('/posts', function (req,res) {
